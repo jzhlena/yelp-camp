1f38942b98cc324747597309d18b30ef326ef054
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-08-17 22:21:46 -0400

    layout tutorial obsolete as express no longer supports

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-
-const Campground = require('./models/campground')
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 });
@@ -16,6 +16,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
