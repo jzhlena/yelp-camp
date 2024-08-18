b2581e7e30f822db69b0ef6aeea45b5d14ba2618
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-08-18 10:16:31 -0400

    update objects with img/description,  visuals on edit/new/show

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            image: 'https://images.unsplash.com/photo-1518602164578-cd0074062767?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'lorem ipsum dolor',
+            price
         })
         await camp.save();
     }
