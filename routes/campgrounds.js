f56d18264decfad085bd1d4fc909b5ac2f8464b0
starlysamax <149339085+starlysamax@users.noreply.github.com>
2024-08-26 00:02:19 -0400

    replaced with semi-styled versions for ease of use, edit + add has issues

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,4 +1,4 @@
-const express=require('express');
+const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
@@ -12,7 +12,7 @@ const validateCampground = (req, res, next) => {
         throw new ExpressError(msg, 400);
     }
     else {
-        next()
+        next();
     }
 }
 
@@ -26,8 +26,6 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
